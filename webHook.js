bef709b4e59c94a187ae161b40d881b0b86526b0
ankith <20mcme19@uohyd.ac.in>
2023-05-23 09:31:19 +0530

    first commit

--- webHook.js
+++ webHook.js
@@ -71,7 +71,7 @@ app.post('/webhook',async (req,res)=>{
                     }
                 }).then(function (response) {
                     console.log(`Response: ${response.status} ${response.statusText}`);
-                    return response.text;
+                    return response.text();
                   }).then(text => console.log(text))
                   .catch(function (error) {
                     console.log(error);
