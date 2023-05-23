c788379e6a2c3bddbe9dcda3d691ad8c8490cd4b
ankith <20mcme19@uohyd.ac.in>
2023-05-23 17:47:05 +0530

    first commit

--- webHook.js
+++ webHook.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const body_parser = require('body-parser');
 const axios = require('axios');
-const { response } = require('express');
 require('dotenv').config();
 
-const app = express().use(bodyparser.json())
+const app = express().use(body_parser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000 ;
 const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;
 
@@ -37,52 +36,44 @@ app.post('/webhook', async (req, res) => {
 
     //console.log(JSON.stringify(body_param,null,2));
     if (body_param.object) {
-        
         if (body_param.entry &&
             body_param.entry[0].changes &&
             body_param.entry[0].changes[0].value.messages &&
             body_param.entry[0].changes[0].value.messages[0]
         ) {
-            
             const phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             const from = body_param.entry[0].changes[0].value.messages[0].from;
             const msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
             const key = `Basic ${Buffer.from(`${process.env.MAIL}: ${process.env.JIRA_API_KEY}`).toString('base64')}`;
 
             try {
-               
                 const response = await axios.post(
                     "https://coolsite42.atlassian.net/rest/api/3/issue",
                     {
-                        "fields": {
-                            "summary": `${msg_body}`,
-                            "issuetype": {
+                            "fields": {
+                              "summary": msg_body,
+                              "issuetype": {
                                 "id": "10001"
+                              },
+                              "project":{
+                                "id":"10000"
+                              },
                             },
-                            "project": {
-                                "id": "10000"
-                            },
-                        },
-                    }, {
+                    },{
                     headers: {
                         'Authorization': key,
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'
                     },
-                });
-                console.log("try catch entered");
-                console.log(`Response from Jira: ${response.status} ${response.statusText}`);
-                console.log(response.data);
-
+                }) ;
+                console.log(`Response from Jira: ${response.status} ${response.statusText}`) ;
+                console.log(responsez)
                 res.status(200).send("Request success");
-            } catch (err) {
-                console.log(err);
-                res.status(501).send('An error occured while sending the message to jira');
             }
         } else {
             res.sendStatus(404);
         }
     }
-});
+})
 
 app.listen(process.env.PORT, () => console.log(`WebHook listening on port ${port}!`))
